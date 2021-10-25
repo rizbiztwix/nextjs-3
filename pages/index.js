041dca1e151b6674e18e4522082bbcc3126a097e
Riz <rizwana.akmal@hotmail.com>
2021-10-25 13:35:00 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! test - 2</a>
+          Welcome to <a href="https://nextjs.org">Next.js! test - 3</a>
         </h1>
 
         <p className={styles.description}>
